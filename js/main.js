08eca86a930d451db8590b90a7bfeaa16a5aa2f2
shahobidin <temirovshahobiddin@gmail.com>
2022-03-20 13:17:38 +0300

    adding new javascript element

---
+++ js/main.js
@@ -0,0 +1,47 @@
+
+var x = prompt("karochi hurmat yuzasidan ismizi etvorolismi oka");
+
+var y = prompt(" uzur oka hurmat yuzasidan yoshiziyam  etvorin oka");
+
+alert("Oka misollani yechvorin matematikadan qiynalvotudim");
+
+var misol1 = prompt("5 + 9 = ...");
+
+var misol2 = prompt("9 + 9 = ...");
+
+var misol3 = prompt("15 + 39 = ...");
+
+var misol4 = prompt("52 + 22 = ...");
+
+var misol5 = prompt("34 + 39 = ...");
+
+
+
+alert("oka jovoblani bilvolish uchun konsolega abrashatsa qvorin");
+
+
+
+
+
+
+
+
+
+//console 
+console.log("Men ismim " + x );
+
+console.log("Men " + y + " yoshdaman");
+
+console.log(" sizning javobingiz 5 + 9 = " + misol1 + " to'gri javob ") + (5 + 9);
+
+console.log(" sizning javobingiz 9 + 9 = " + misol2 + " to'gri javob " + (9 + 9));
+
+console.log(" sizning javobingiz 15 + 39 = " + misol3 + " to'gri javob ") + (15 + 39);
+
+console.log(" sizning javobingiz 52 + 22 = " + misol4 + " to'gri javob ") + (52 + 22);
+
+console.log(" sizning javobingiz 34 + 39 = " + misol5 + " to'gri javob ") + (34 + 39);
+
+
+
+
